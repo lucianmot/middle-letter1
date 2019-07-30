d1717ea4d8d154069c4e6351a68b03541527c91b
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-30 11:12:30 +0100

    done

--- spec/get_middle_spec.rb
+++ spec/get_middle_spec.rb
@@ -1,22 +1,22 @@
 require 'get_middle'
 
 describe Middle do
-    it "when you input a word it should return that word" do
+    it "when you input a letter it should return that letter" do
       something = Middle.new
     expect(something.print("A")).to eq("A")
     end
 
-    it "when you input a word it should return that word" do
+    it "when you input two letters it should return those two letters" do
       something = Middle.new
       expect(something.print("of")).to eq("of")
     end
 
-    it "when you input a word it should return that word" do
+    it "when you input a even letter word it should return the middle letters" do
       something = Middle.new
       expect(something.print("test")).to eq("es")
     end
 
-    it "when you input a word it should return that word" do
+    it "when you input a odd letter word it should return the middle letter" do
       something = Middle.new
       expect(something.print("testing")).to eq("t")
     end
